d0cc480616618018e9ee22414cdd8bab276f0455
captainaomi <126124875+captainaomi@users.noreply.github.com>
2023-09-25 22:11:12 +1000

    created home page and about page routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+// React Router parts:
+// - BrowserRouter needs to exist higher up in the component tree than whatever we are writing Routes in
+// - Routes need to exist, determine what content is rendered on which route eg. /about for <anout/>
+// - Route specifies URL and component, belongs to Routes
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import BusinessCardParent from './BusinessCardParent';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
 
 function App() {
   return (
     <div className="App">
-      <BusinessCardParent />
-      <BusinessCardParent />
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        
+        <Route path="/about" element={<AboutPage />} />
+
+      </Routes>
+
     </div>
   );
 }
---
+++ src/pages/BusinessCardPage.jsx
@@ -0,0 +1,12 @@
+import BusinessCardParent from './BusinessCardParent';
+
+export default function BusinessCardPage(props){
+    return(
+        <div>
+            <h1>Business Cards go here eventually</h1>
+            <BusinessCardParent />
+            <BusinessCardParent />
+            <BusinessCardParent />
+        </div>
+    )
+}
