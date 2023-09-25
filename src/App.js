75b0e181f03e03a632fd15143071db142daa74c0
captainaomi <126124875+captainaomi@users.noreply.github.com>
2023-09-26 09:59:18 +1000

    navbar and components

--- src/App.js
+++ src/App.js
@@ -3,19 +3,47 @@
 // - Routes need to exist, determine what content is rendered on which route eg. /about for <anout/>
 // - Route specifies URL and component, belongs to Routes
 
-import { Route, Routes } from 'react-router-dom';
+import { Outlet, Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+
+      <h1>This h1 shows on every page</h1>
+      <Navbar />
+      
       <Routes>
         <Route path="/" element={<HomePage/>} />
-        
-        <Route path="/about" element={<AboutPage />} />
 
+        {/* Route for /about opening - BUT WITH AN EXTRA SLASH; STILL VALID, BUT GENERALLY DO BELOW
+        <Route path="/about" element={<AboutPage />}>*/}
+
+          {/* Route for /about/contact
+          <Route path="/about/contact" element={<ContactPage/>} /> */}
+
+        {/* Route for /about closing
+        </Route> */}
+
+
+        {/* Route for /about opening */}
+        {/* <Route path="about" element={<AboutPage />}> */}
+
+          {/* Route for /about/contact */}
+          {/* <Route path="contact" element={<ContactPage/>} /> */}
+
+        {/* Route for /about closing */}
+        {/* </Route> */}
+
+
+        <Route path="/about" element={<Outlet />}>
+          <Route index element={<AboutPage />} />
+          <Route path="contact" element={<ContactPage />} />
+        </Route>
       </Routes>
 
     </div>
